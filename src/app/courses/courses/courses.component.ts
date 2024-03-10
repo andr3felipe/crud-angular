0bf9269d02de45d1b3c148949261f96bd0368fa4
andr3felipe <andre.felipe_680@hotmail.com>
2024-03-10 18:45:39 -0300

    add: card, courses module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'Front-end',
+    },
+  ];
+  displayedColumns = ['name', 'category'];
 
+  ngOnInit(): void {}
 }
